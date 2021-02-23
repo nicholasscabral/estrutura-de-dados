f7727ca51ee583b5a4fbfd7c35837ebc80ef5234
nicholasscabral <nicholasscabral@gmail.com>
2021-02-23 12:46:24 -0300

    ListaEstatica class structure

---
+++ lista/src/ListaEstatica.java
@@ -0,0 +1,35 @@
+public class ListaEstatica {
+    private int v[];
+
+    public ListaEstatica() {
+        v = new int[10];
+    }
+
+    public ListaEstatica(int n) {
+        v = new int[n];
+    }
+
+    public void adicionar(int valor) {
+
+    }
+
+    public void adicionar(int posicao, int valor) {
+
+    }
+
+    public int tamanho() {
+
+    }
+
+    public boolean pesquisar(int valor) {
+
+    }
+
+    public int pesquisar(int posicao) {
+
+    }
+
+    public void remover(int valor) {
+
+    }
+}

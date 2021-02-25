9bb479240117c7b5e822354735758fbb9c89d599
nicholasscabral <nicholasscabral@gmail.com>
2021-02-25 11:51:54 -0300

    adicionar valor e exibir lista (ListaEstatica)

--- lista/src/ListaEstatica.java
+++ lista/src/ListaEstatica.java
@@ -1,8 +1,10 @@
 public class ListaEstatica {
     private int v[];
+    private int contador;
 
     public ListaEstatica() {
         v = new int[10];
+        contador = 0;
     }
 
     public ListaEstatica(int n) {
@@ -10,7 +12,8 @@ public class ListaEstatica {
     }
 
     public void adicionar(int valor) {
-
+        v[contador] = valor;
+        contador++;
     }
 
     public void adicionar(int posicao, int valor) {
@@ -31,5 +34,10 @@ public class ListaEstatica {
 
     public void remover(int valor) {
 
+    }
+
+    public void exibir() {
+        for (int i = 0; i < contador; i++) System.out.print(v[i] + " ");
+        System.out.println();
     }
 }
--- lista/src/Main.java
+++ lista/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         ListaEstatica lista = new ListaEstatica();
+
+        lista.adicionar(7);
+        lista.adicionar(9);
+        lista.exibir();
     }
 }

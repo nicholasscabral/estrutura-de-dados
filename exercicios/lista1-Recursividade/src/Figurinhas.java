fa22b60ab60fb893497d11166d906c10b1a2852b
nicholasscabral <nicholasscabral@gmail.com>
2021-03-04 19:25:58 -0300

    solução Figurinhas

--- exercicios/lista1-Recursividade/src/Figurinhas.java
+++ exercicios/lista1-Recursividade/src/Figurinhas.java
@@ -3,27 +3,23 @@ import java.util.Scanner;
 public class Figurinhas {
 
     public static int mdc(int x, int y) {
-        if (x % y == 0)
-            return y;
-        else
-            return mdc(y, x % y);
+        if (y == 0) return x;
+        int resto = x % y;
+        x = y;
+        y = resto;
+        return mdc(x, y);
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int cases = input.nextInt();
-        if (1 <= cases && cases <= 3000) {
-            for (int i = 0; i < cases; i++) {
-                int ricardo = input.nextInt();
-                int vicente = input.nextInt();
-                if (1 <= ricardo && ricardo <= 1000 && 1000 >= vicente && vicente >= 1) {
-                    if (ricardo > vicente)
-                        System.out.println(mdc(ricardo, vicente));
-                    else if (vicente > ricardo)
-                        System.out.println(mdc(vicente, ricardo));
-                }
-            }
+        for (int i = 0; i < cases; i++) {
+            int x = input.nextInt();
+            int y = input.nextInt();
+
+            System.out.println(mdc(x, y));
         }
+
     }
 }

c9b5743fe2d87c3c04941e90fbdcd1b3cd201f8e
nicholasscabral <nicholasscabral@gmail.com>
2021-05-18 12:52:58 -0300

    ArvoreBinaria

---
+++ src/arvore/src/ArvoreBinaria.java
@@ -0,0 +1,23 @@
+public class ArvoreBinaria {
+    private No raiz;
+
+    public ArvoreBinaria() {
+        this.raiz = null;
+    }
+
+    public void add(Object e) {
+
+    }
+
+    public void remove(Object e) {
+
+    }
+
+    public boolean contains(Object e) {
+        return false;
+    }
+
+    public void list() {
+
+    }
+}
---
+++ src/arvore/src/No.java
@@ -0,0 +1,11 @@
+public class No {
+    public Object dado;
+    public No esquerdo;
+    public No direito;
+
+    public No(Object dado) {
+        this.dado = dado;
+        this.esquerdo = null;
+        this.direito = null;
+    }
+}
